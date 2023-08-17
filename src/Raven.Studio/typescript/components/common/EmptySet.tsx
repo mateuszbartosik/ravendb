02729c9181335947e64aac34b85efede85586767
Mateusz <mateusz@ravendb.net>
2023-08-17 15:49:07 +0200

    RavenDB-21129 Move Custom Sorters and Custom Analyzers to React

--- src/Raven.Studio/typescript/components/common/EmptySet.tsx
+++ src/Raven.Studio/typescript/components/common/EmptySet.tsx
@@ -1,19 +1,21 @@
 ﻿import React, { ReactNode } from "react";
 import { Icon } from "./Icon";
 import IconName from "typings/server/icons";
+import classNames from "classnames";
 
 interface EmptySetProps {
     children?: ReactNode | ReactNode[];
     icon?: IconName;
     color?: string | "muted";
+    className?: string;
 }
 
 export function EmptySet(props: EmptySetProps) {
-    const { children, icon, color } = props;
+    const { children, icon, color, className } = props;
     const defaultIcon: IconName = "empty-set";
 
     return (
-        <div className="empty-set text-center mb-2 mx-auto">
+        <div className={classNames("empty-set text-center mb-2 mx-auto", className)}>
             <Icon icon={icon || defaultIcon} color={color} className="icon-xl" margin="m-0" />
             <div className="lead">{children}</div>
         </div>
